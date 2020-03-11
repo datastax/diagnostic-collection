dbe1384703f986f103ad79996bed974802c3006a
Alex Ott <alexey.ott@datastax.com>
2020-03-11 09:36:57 +0100

    Parallel collection of diagnostic data from nodes
    this fixes #23

--- collect_diag.sh
+++ collect_diag.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # File: collect_diag.sh
 #
 # Created: Friday, May 31 2019
 ##
+
+if [ "${BASH_VERSINFO[0]}" -lt 4 ]; then
+    echo "You need to use Bash 4 or higher, but you have ${BASH_VERSION}"
+    exit 1
+fi
 
 function usage() {
     echo "Usage: $0 -t <type> [options] [path]"
@@ -124,7 +129,9 @@ fi
 
 # TODO: calculate ServerAliveCountMax based on the timeout & ServerAliveInterval...
 SSH_OPTS="$SSH_OPTS -o StrictHostKeyChecking=no -o ConnectTimeout=$TIMEOUT -o BatchMode=yes -o ServerAliveInterval=15 -o ServerAliveCountMax=40"
-for host in $(cat $HOST_FILE); do
+
+declare -A servers
+for host in $(cat "$HOST_FILE"); do
     echo "Copying collect_node_diag.sh to $host..."
     scp $SSH_OPTS collect_node_diag.sh "${host}:~/"
     RES=$?
@@ -132,18 +139,61 @@ for host in $(cat $HOST_FILE); do
         echo "Error during execution SCP, copying script to host $host, exiting..."
         exit 1
     fi
-    NODE_OUT_DIR="$(ssh $SSH_OPTS $host 'mktemp -d'| tr -d '\r')"
-    ssh $SSH_OPTS $host "bash --login ./collect_node_diag.sh -t $TYPE -o $NODE_OUT_DIR $COLLECT_OPTS $INSIGHT_COLLECT_OPTS -c '$CQLSH_OPTS' -n '$NT_OPTS' -d '$DT_OPTS' '$DSE_DDAC_ROOT'"
-    RES=$?
+    NODE_OUT_DIR="$(ssh $SSH_OPTS "$host" 'mktemp -d'| tr -d '\r')"
     if [ $RES -ne 0 ]; then
-        echo "Error during execution SSH, exiting..."
+        echo "Error creating a temp directory on host $host, exiting..."
         exit 1
     fi
+    servers[$host]=$NODE_OUT_DIR
+    echo "host: $host out_dir=$NODE_OUT_DIR"
+done
+    
+declare -A pids
+for host in "${!servers[@]}"; do
+    NODE_OUT_DIR="${servers[$host]}"
+    ssh $SSH_OPTS $host "bash --login ./collect_node_diag.sh -t $TYPE -o $NODE_OUT_DIR $COLLECT_OPTS $INSIGHT_COLLECT_OPTS -c '$CQLSH_OPTS' -n '$NT_OPTS' -d '$DT_OPTS' '$DSE_DDAC_ROOT'" &
+    pids[$host]="${!}"
+done
+
+declare -a hosts_failed
+declare -a hosts_success
+for host in "${!pids[@]}"; do
+    echo "Going to wait for PID ${pids[$host]} for host $host"
+    if wait ${pids[$host]}; then
+        RES=$?
+        if [ "$RES" -eq 0 ]; then
+            hosts_success+=("$host")
+        else
+            hosts_failed+=("$host")
+        fi
+    else
+        hosts_failed+=("$host")
+    fi
+done
+
+failed_len=${#hosts_failed[@]}
+if [ "$failed_len" -gt 0 ]; then
+    if [ "$failed_len" -eq ${#servers[@]} ]; then
+        echo "Collection failed on all hosts!"
+        exit 1
+    else
+        echo "Collection failed on $failed_len hosts: ${hosts_failed[*]}"
+        echo "We will generate diagnostic tarball only for hosts where collection was successful"
+    fi
+fi
+
+# we continue to 
+for host in "${hosts_success[@]}"; do
+    NODE_OUT_DIR="${servers[$host]}"
+    echo "host: $host out_dir=$NODE_OUT_DIR"
     scp $SSH_OPTS "${host}:${NODE_OUT_DIR}/*.tar.gz" "$OUT_DIR"
     RES=$?
     if [ $RES -ne 0 ]; then
         echo "Error during execution SCP, copying data from host $host, exiting..."
         exit 1
+    fi
+    if [ -n "$REMOVE_OPTS" ]; then
+        ssh $SSH_OPTS "$host" "rm -rf '$NODE_OUT_DIR'"
     fi
 done
 

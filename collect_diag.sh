068cfe634b97cd1fd0c884ca88ca97b8c89d760e
brendancicchi <bcicchi@gmail.com>
2020-09-29 11:51:22 -0700

    Collect SSL store data * Add -k flag to opt in for collection * Leverage the existing cassandra.yaml properties to formulate keytool command * Store under conf/security/ for each node * Only collect where applicable i.e. client_encryption and/or server_encryption enabled

--- collect_diag.sh
+++ collect_diag.sh
@@ -22,6 +22,7 @@ function usage() {
     echo "   -c cqlsh_options - e.g \"-u user -p password\" etc. Ensure you enclose with \""
     echo "   -d dsetool_options - options to pass to dsetool. Syntax the same as \"-c\""
     echo "   -f file_name - file with list of hosts where to execute command (default - try to get list from 'nodetool status')"
+    echo "   -k keystore_ssl_info - collect keystore and truststore information"   
     echo "   -i insights - collect only data for DSE Insights"
     echo "   -I insights_dir - directory that contains insights .gz files"
     echo "   -n nodetool_options - options to pass to nodetool. Syntax the same as \"-c\""
@@ -73,13 +74,15 @@ TYPE=""
 # Parse arguments
 # ---------------
 
-while getopts ":hzivrn:c:d:f:o:p:s:t:u:I:m:" opt; do
+while getopts ":hzivrkn:c:d:f:o:p:s:t:u:I:m:" opt; do
     case $opt in
         c) CQLSH_OPTS="$OPTARG"
            ;;
         d) DT_OPTS="$OPTARG"
            ;;
         f) HOST_FILE="$OPTARG"
+           ;;
+        k) COLLECT_OPTS="$COLLECT_OPTS -k"
            ;;
         i) COLLECT_OPTS="$COLLECT_OPTS -i"
            ;;
